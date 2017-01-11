6eec0c979ca7af9f6c015f6536e165d70acbb440
arpercussion <anthony_romano@percussion.com>
2017-01-11 17:09:41 -0500

    allow add/remove of domain after config

--- post-message.js
+++ post-message.js
@@ -1,134 +1,151 @@
 /**
-@fileOverview
+ @fileOverview
 
-@toc
+ @toc
 
-*/
+ */
 
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('petarslovic.ng-post-message', [])
-    .provider('PostMessage', PostMessageProvider);
+    angular
+        .module('petarslovic.ng-post-message', [])
+        .provider('PostMessage', PostMessageProvider);
 
-  function PostMessageProvider() {
-    var allowedDomains = [];
-    var debug = {
-      on: false
-    };
-
-    this.setAllowedDomains = function(newAllowedDomains) {
-      allowedDomains = newAllowedDomains;
-    };
-
-    this.debug = function(shouldDebug) {
-      debug.on = shouldDebug;
-    };
-
-    this.$get = function($window, $rootScope, $log) {
-      var callbacks = {};
-
-      var api = {
-        send: sendMessage,
-        on: onMessage
-      };
-
-      angular.element(window).bind('message', onPostMessage);
-
-      return api;
-
-      function sendMessage(messageName, data, iframeName, domain) {
-        var sender;
-        var serializedData = serializeData(messageName, data);
-
-        if(domain) {
-          if(allowedDomains.indexOf(domain) === -1) {
-            throw new Error('Sending messages to domain ' + domain + ' must be explicitly allowed.');
-          }
-        } else {
-          domain = '*';
-        }
-
-        var iframe = $window.document.querySelector('iframe[name="' + iframeName + '"]');
-
-        sender = iframe ? iframe.contentWindow : $window.parent;
-
-        if(debug.on) {
-          $log.info('ng-post-message: sendMessage', messageName, data, iframeName, domain);
-        }
-
-        sender.postMessage(serializedData, domain);
-      }
-
-      function onMessage(messageName, callback) {
-        if(!callbacks[messageName]) {
-          callbacks[messageName] = [];
-        }
-
-        callbacks[messageName].push(callback);
-
-        if(debug.on) {
-          $log.info('ng-post-message: registered callback for ', messageName);
-        }
-
-        return offMessage(messageName, callback);
-      }
-
-      function offMessage(messageName, callback) {
-        return function() {
-          callbacks[messageName] = _.filter(callbacks[messageName], function(cb) {
-            return cb !== callback;
-          });
-
-          if(debug.on) {
-            $log.info('ng-post-message: removed callback for', messageName);
-          }
+    function PostMessageProvider() {
+        var allowedDomains = [];
+        var debug = {
+            on: false
         };
-      }
 
-      function onPostMessage(event) {
-        var origin = event.origin || (event.originalEvent && event.originalEvent.origin) || null;
-        var data = event.data || (event.originalEvent && event.originalEvent.data) || 'null';
-
-        if(allowedDomains.indexOf(origin) === -1) {
-          throw new Error('Receiving messages from domain ' + origin + ' must be explicitly allowed.');
-        }
-
-        var parsedData = typeof data === 'string' ? parseData(data) : data;
-        var messageName = parsedData.messageName || parsedData.message;
-
-        // Don't allow Batarang messages to slow the app down :)
-        if(parsedData.__fromBatarang) {
-          return;
-        }
-
-        if(callbacks[messageName] && callbacks[messageName].length) {
-          if(debug.on) {
-            $log.info('ng-post-message: onMessage', messageName, parsedData, origin);
-          }
-
-          _.invoke(callbacks[messageName], _.call, null, event, parsedData);
-          $rootScope.$digest();
-        }
-      }
-
-      function parseData(data) {
-        if(!data || typeof data !== 'string') {
-          return {};
-        }
-
-        var parts = data.split(/:(.+)/);
-        return {
-          messageName: parts[0],
-          data: angular.fromJson(parts[1])
+        this.setAllowedDomains = function (newAllowedDomains) {
+            allowedDomains = newAllowedDomains;
         };
-      }
 
-      function serializeData(messageName, data) {
-        return messageName + ':' + angular.toJson(data);
-      }
-    };
+        this.debug = function (shouldDebug) {
+            debug.on = shouldDebug;
+        };
 
-  }
+        this.$get = function ($window, $rootScope, $log) {
+            var callbacks = {};
+
+            var api = {
+                send: sendMessage,
+                on: onMessage,
+                addDomain: addDomain,
+                removeDomain: removeDomain
+            };
+
+            angular.element(window).bind('message', onPostMessage);
+
+            return api;
+
+            function addDomain(domain) {
+                var index = allowedDomains.findIndex(function (el) {
+                    return el.toLowerCase() === domain.toLowerCase();
+                });
+                if (index == -1) {
+                    allowedDomains.push(domain);
+                }
+            }
+
+            function removeDomain(domain) {
+                allowedDomains = allowedDomains.filter(function (el) {
+                    return el.toLowerCase() !== domain.toLowerCase();
+                });
+            }
+
+            function sendMessage(messageName, data, iframeName, domain) {
+                var sender;
+                var serializedData = serializeData(messageName, data);
+
+                if (domain) {
+                    if (allowedDomains.indexOf(domain) === -1) {
+                        throw new Error('Sending messages to domain ' + domain + ' must be explicitly allowed.');
+                    }
+                } else {
+                    domain = '*';
+                }
+
+                var iframe = $window.document.querySelector('iframe[name="' + iframeName + '"]');
+
+                sender = iframe ? iframe.contentWindow : $window.parent;
+
+                if (debug.on) {
+                    $log.info('ng-post-message: sendMessage', messageName, data, iframeName, domain);
+                }
+
+                sender.postMessage(serializedData, domain);
+            }
+
+            function onMessage(messageName, callback) {
+                if (!callbacks[messageName]) {
+                    callbacks[messageName] = [];
+                }
+
+                callbacks[messageName].push(callback);
+
+                if (debug.on) {
+                    $log.info('ng-post-message: registered callback for ', messageName);
+                }
+
+                return offMessage(messageName, callback);
+            }
+
+            function offMessage(messageName, callback) {
+                return function () {
+                    callbacks[messageName] = _.filter(callbacks[messageName], function (cb) {
+                        return cb !== callback;
+                    });
+
+                    if (debug.on) {
+                        $log.info('ng-post-message: removed callback for', messageName);
+                    }
+                };
+            }
+
+            function onPostMessage(event) {
+                var origin = event.origin || (event.originalEvent && event.originalEvent.origin) || null;
+                var data = event.data || (event.originalEvent && event.originalEvent.data) || 'null';
+
+                if (allowedDomains.indexOf(origin) === -1) {
+                    throw new Error('Receiving messages from domain ' + origin + ' must be explicitly allowed.');
+                }
+
+                var parsedData = typeof data === 'string' ? parseData(data) : data;
+                var messageName = parsedData.messageName || parsedData.message;
+
+                // Don't allow Batarang messages to slow the app down :)
+                if (parsedData.__fromBatarang) {
+                    return;
+                }
+
+                if (callbacks[messageName] && callbacks[messageName].length) {
+                    if (debug.on) {
+                        $log.info('ng-post-message: onMessage', messageName, parsedData, origin);
+                    }
+
+                    _.invoke(callbacks[messageName], _.call, null, event, parsedData);
+                    $rootScope.$digest();
+                }
+            }
+
+            function parseData(data) {
+                if (!data || typeof data !== 'string') {
+                    return {};
+                }
+
+                var parts = data.split(/:(.+)/);
+                return {
+                    messageName: parts[0],
+                    data: angular.fromJson(parts[1])
+                };
+            }
+
+            function serializeData(messageName, data) {
+                return messageName + ':' + angular.toJson(data);
+            }
+        };
+
+    }
 })();
